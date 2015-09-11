7781540386c186a4b9ba8eeaae5b74608e5b1c21
Simon Meier <simonmeier@Apples-MacBook-Pro.local>
2015-09-11 11:47:51 +0200

    MountainRaid

---
+++ hackathon2015-ios WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,37 @@
+//
+//  InterfaceController.swift
+//  hackathon2015-ios WatchKit Extension
+//
+//  Created by Simon Meier on 11/09/15.
+//  Copyright (c) 2015 itcc. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+    override func awakeWithContext(context: AnyObject?) {
+        super.awakeWithContext(context)
+        
+        // Configure interface objects here.
+    }
+
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+    
+    override func contextForSegueWithIdentifier(segueIdentifier: String) -> AnyObject? {
+        if segueIdentifier == "showMountain" {
+            return ["segue": "showMountain", "data": "Passed through push"]
+        } else {
+            return ["segue": "", "data": ""]
+        }
+    }
+}
